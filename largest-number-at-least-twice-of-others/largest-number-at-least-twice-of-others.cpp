b50316e5d03aa7afccfe9b56874eb243ee958700
Sandhya Kumari <85842071+Sandhya312@users.noreply.github.com>
2023-02-08 11:40:02 +0530

    Time: 3 ms, Memory: 10.8 MB - LeetHub

--- largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
+++ largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
@@ -17,23 +17,4 @@ public:
         return idx;
     }
 };
-// int n = nums.size() ;
-//         int lar = nums[0];
-//         int in = 0 ;
-//         for(int i=1 ; i<n ; i++)
-//         {
-//            if(lar<nums[i])
-//            {
-//                lar = nums[i] ;
-//                in = i ;
-//            }
-//         }   
-//         for(int j=0 ; j<n ; j++)
-//         {
-//             if(in!=j && nums[j]>lar/2)
-//             {
-//                return -1 ;
-//             }
-//         }
-//         return in ;
 
