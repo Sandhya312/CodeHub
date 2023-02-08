58c06e898bd82d1452f7edb6434a53cdac229fbd
Sandhya Kumari <85842071+Sandhya312@users.noreply.github.com>
2023-02-08 11:42:07 +0530

    Time: 6 ms, Memory: 10.9 MB - LeetHub

--- largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
+++ largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int dominantIndex(vector<int>& nums) {
-        int max = nums[0];
-        int idx = 0;
+        int max=nums[0];
+        int ans=0;
         for(int i=1;i<nums.size();i++){
             if(nums[i]>max){
-                max = nums[i];
-                idx=i;
+                max=nums[i];
+                ans=i;
             }
         }
-        for(int j=0;j<nums.size();j++){
-            if(max !=nums[j]){
-                if(max<2*nums[j]) return -1;
+        for(int i=0;i<nums.size();i++){
+            if(max !=nums[i]){
+                if( max<2*nums[i]) return -1;
             }
+            
         }
-        return idx;
+        return ans;
     }
 };
 
